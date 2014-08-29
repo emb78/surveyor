617b0e8ad685f8f50ae6aecf8ecff3b39f765c9f
Beth Brown <bbrown@cyrusinnovation.com>
2014-08-29 16:05:12 -0400

    Fix controller test

--- spec/controllers/surveys_controller_spec.rb
+++ spec/controllers/surveys_controller_spec.rb
@@ -13,12 +13,12 @@ describe SurveysController do
   describe 'create' do
     it "should redirect to index when creation successful" do
       allow_any_instance_of(Survey).to receive(:valid?).and_return(true)
-      post 'create', :title => "Best Survey Ever"
+      post 'create', survey: {title: "Best Survey Ever"}
       response.should redirect_to(surveys_path)
     end
 
     it "should stay on form when creation unsuccessful" do
-      post 'create'
+      post 'create', survey: {title: ""}
       response.should render_template('new')
     end
   end
